3a95239c33639b0c231a7a22b34dce907014637a
Hailie17 <192151452@qq.com>
2024-08-16 02:38:41 +0800

    3.2 supperagent获取页面内容

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,9 +1,15 @@
+import superagent from 'superagent'
 class Crowller {
   private secret = 'secretKey'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
+  private rawHtml = ''
+
+  async getRawHtml() {
+    const result = await superagent.get(this.url)
+    this.rawHtml = result.text
+  }
   constructor () {
-    console.log('hello');
-    // 11
+    this.getRawHtml()
   }
 }
 
