5a8268e792063d94d8f67fb9345ed8abc2dadab6
Hailie17 <192151452@qq.com>
2024-08-19 00:30:29 +0800

    拆分为2个类

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,66 +1,35 @@
+/**
+ * 存文件内容
+ */
+
 import fs from 'fs'
 import path from 'path'
 import superagent from 'superagent'
-import * as cheerio from 'cheerio';
-import { data } from 'cheerio/dist/commonjs/api/attributes';
+import DellAnalyzer from './dellAnalyzer'
 
-interface Course {
-  title: string,
-  count: number
-}
-interface CourseResult {
-  time: number,
-  data: Course[]
-}
-interface Content {
-  [propName: number]: Course[]
-}
+
 class Crowller {
   private secret = 'secretKey'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
   private filePath = path.resolve(__dirname,'../data/course.json')
-  // 拿到课程信息
-  getCourseInfo(html: string){
-    const $ = cheerio.load(html)
-    const courseItems = $('.course-item')
-    const courseInfos:Course[] = []
-    courseItems.map((index,element)=> {
-      const descs = $(element).find('.course-desc')
-      const title = descs.eq(0).text()
-      const count = parseInt(descs.eq(1).text().split('：')[1],10)
-      courseInfos.push({title,count})
-    })
-    const result = {
-      time: new Date().getTime(),
-      data: courseInfos
-    }   
-    return result 
-  }
+  
   // 拿到原始html
   async getRawHtml() {
     const result = await superagent.get(this.url)
     return result.text
   }
-  // 生成json
-  generateJsonContent(courseInfo: CourseResult) {
-    let fileContent:Content = {}
-    if(fs.existsSync(this.filePath)){
-      fileContent = JSON.parse(fs.readFileSync(this.filePath,'utf-8'))
-    }
-    fileContent[courseInfo.time] = courseInfo.data
-    return fileContent
-  }
+  
   // 爬取
   async initSpiderProcess() {
     const html = await this.getRawHtml()
-    const courseInfo = this.getCourseInfo(html)
-    const fileContent = this.generateJsonContent(courseInfo)
+    const fileContent = this.analyze.analyze(html, this.filePath)
     // 写入文件
     fs.writeFileSync(this.filePath,JSON.stringify(fileContent))
   }
-  constructor () {
+  constructor (private analyze: any) {
     this.initSpiderProcess()
   }
 }
 
-const crowller = new Crowller()
+const analyze = new DellAnalyzer()
+const crowller = new Crowller(analyze)
--- src/dellAnalyzer.ts
+++ src/dellAnalyzer.ts
@@ -1,3 +1,51 @@
-class DellAnalyzer {
-  
+/**
+ * 分析页面拿到内容
+ */
+import * as cheerio from 'cheerio';
+import { data } from 'cheerio/dist/commonjs/api/attributes';
+import fs from 'fs'
+interface Course {
+  title: string,
+  count: number
+}
+interface CourseResult {
+  time: number,
+  data: Course[]
+}
+interface Content {
+  [propName: number]: Course[]
+}
+export default class DellAnalyzer {
+  // 拿到课程信息
+  private getCourseInfo(html: string){
+    const $ = cheerio.load(html)
+    const courseItems = $('.course-item')
+    const courseInfos:Course[] = []
+    courseItems.map((index,element)=> {
+      const descs = $(element).find('.course-desc')
+      const title = descs.eq(0).text()
+      const count = parseInt(descs.eq(1).text().split('：')[1],10)
+      courseInfos.push({title,count})
+    })
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos
+    }   
+    return result 
+  }
+  // 生成json
+  generateJsonContent(courseInfo: CourseResult,filePath:string) {
+    let fileContent:Content = {}
+    if(fs.existsSync(filePath)){
+      fileContent = JSON.parse(fs.readFileSync(filePath,'utf-8'))
+    }
+    fileContent[courseInfo.time] = courseInfo.data
+    return fileContent
+  }
+
+  public analyze(html:string, filePath:string) {
+    const courseInfo = this.getCourseInfo(html)
+    const fileContent = this.generateJsonContent(courseInfo,filePath)
+    return JSON.stringify(fileContent)
+  }
 }
