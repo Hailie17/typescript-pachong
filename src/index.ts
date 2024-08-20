51595ee332fbacd431dbe55fe2b28722150c8336
Hailie17 <192151452@qq.com>
2024-08-20 23:54:25 +0800

    cookie-session 插件使用

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, {Request, Response} from 'express'
 import bodyParser from 'body-parser'
 import router from './router'
+import cookieSession from 'cookie-session'
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['teacher dell'],
+    maxAge: 24 * 60 * 60 * 1000
+  })
+)
+
 app.use(router)
-
-
 app.listen(7001, () => {
   console.log('server is running');
 })
