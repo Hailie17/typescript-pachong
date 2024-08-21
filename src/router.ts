9d8feae881f45c942ff24f722ca1621b3177215d
Hailie17 <192151452@qq.com>
2024-08-21 23:47:40 +0800

    11

--- src/router.ts
+++ src/router.ts
@@ -16,6 +16,7 @@ router.get('/', (req: Request, res: Response) => {
     res.send(`
     <html>
       <body>
+        <a href="/getData">爬取网页</a>
         <a href="/logout">退出</a>
       </body>
     </html>
@@ -41,21 +42,30 @@ router.get('/logout', (req: Request, res: Response) => {
   res.redirect('/')
 })
 
-router.post('/getData', (req: IRequest, res: Response) => {
-  // const secret = 'secretKey'
-  // const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
-  // const analyze = DellAnalyzer.getInstance()
-  // new Crowller(url, analyze)
+router.post('/login', (req: Request, res: Response) => {
   const isLogin = req.session ? req.session.login : false
   if(isLogin) {
-    res.send('已经登录')
+    res.send('已登录')
   } else {
-    if(req.body.password === '123' && req.session){
+    if(req.body.password === '123' && req.session) {
       req.session.login = true
-      res.send('登陆成功')
+      res.send('登录成功')
     } else {
-      res.send('password error')
+      res.send('登录失败')
     }
+  }
+})
+
+router.get('/getData', (req: IRequest, res: Response) => {
+  const isLogin = req.session ? req.session.login : false
+  if(isLogin) {
+    const secret = 'secretKey'
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+    const analyze = DellAnalyzer.getInstance()
+    new Crowller(url, analyze)
+    res.send('爬取成功')
+  } else {
+    res.send('请登录后爬取')
   } 
 })
 
